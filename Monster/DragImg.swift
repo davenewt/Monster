0a1e9b1ab87374380bd139644e1b88f331d40d05
David Stroud <david@dwstroud.com>
2016-02-09 11:11:34 +0000

    Draggable food and heart

---
+++ Monster/DragImg.swift
@@ -0,0 +1,55 @@
+//
+//  DragImg.swift
+//  Monster
+//
+//  Created by David Stroud on 09/02/2016.
+//  Copyright © 2016 David Stroud. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+// in the Identity Inspector for the 'heart' and 'food' img on the storyboard, change the class to this new 'DragImg' subclass, to enable dragging!
+
+// also in the viewcontroller, change the @IBOutlet for heartImg and foodImg to 'DragImg' (no longer 'UIImageView')
+
+// in the attributes inspector for the 'heart' and 'food', enable 'user interaction' checkbox
+
+
+class DragImg: UIImageView {
+    
+    // create a var to hold our starting point
+    var originalPosition: CGPoint!
+    
+    
+    // need to override a couple of things but still need to call the parent initialiser otherwise we will have problems!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // now need to intercept certain events and do something with them
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        originalPosition = self.center // grabs the center point when there's a touch
+    
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    
+        if let touch = touches.first { // grab the very first object in the touches set. Remember it's optional, hence using 'if let' syntax
+            let position = touch.locationInView(self.superview) // grab the location in the superview and store in immutable variable 'position'
+            self.center = CGPointMake(position.x, position.y) // move OUR centre to this new position. Wherever your finger is dragging, move the object there.
+        }
+        
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.center = originalPosition
+        
+    }
+}
