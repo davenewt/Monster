77bc930b6f44a76b3964c8ba7a8a99553a2911bc
David Stroud <david@dwstroud.com>
2016-02-09 13:22:29 +0000

    Added sfx and music. End-game now working.

--- Monster/ViewController.swift
+++ Monster/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -25,7 +26,15 @@ class ViewController: UIViewController {
     
     var penalties = 0
     var timer: NSTimer! // using ! because we know for sure there will always be a value assigned, otherwise things will crash!
+    var monsterHappy: Bool = false
+    var currentItem: UInt32 = 0
+    var monsterAlive: Bool = true
     
+    var musicPlayer: AVAudioPlayer!
+    var sfxBite: AVAudioPlayer!
+    var sfxHeart: AVAudioPlayer!
+    var sfxDeath: AVAudioPlayer!
+    var sfxSkull: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,13 +49,58 @@ class ViewController: UIViewController {
                                                                          // remember the colon \/ needed in this context, if your function needs parameters. Calls the below defined func
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
         
-        startTimer()
+        do {
+            try musicPlayer = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("cave-music", ofType: "mp3")!))
+            try sfxBite = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bite", ofType: "wav")!))
+            try sfxHeart = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("heart", ofType: "wav")!))
+            try sfxDeath = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("death", ofType: "wav")!))
+            try sfxSkull = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("skull", ofType: "wav")!))
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
         
+        musicPlayer.prepareToPlay()
+        sfxBite.prepareToPlay()
+        sfxHeart.prepareToPlay()
+        sfxDeath.prepareToPlay()
+        sfxSkull.prepareToPlay()
+        
+        if monsterAlive {
+            startGame()
+        } else {
+            print("Monster is DEAD. Not gonna do anything!")
+        }
         
     }
 
+    func startGame() {
+        startTimer()
+        musicPlayer.play()
+    }
+    
     func itemDroppedOnCharacter(notif: AnyObject) { // we're not going to use this object, so we don't need to worry about it
-        print("Item dropped on character!")
+        print("Item dropped...")
+        
+        // only do stuff if the monster is alive!
+        if monsterAlive {
+            print("Monster is alive, so now he's happy")
+            monsterHappy = true
+            startTimer()
+            foodImg.alpha = DIM_ALPHA
+            heartImg.alpha = DIM_ALPHA
+            foodImg.userInteractionEnabled = false
+            heartImg.userInteractionEnabled = false
+            
+            if currentItem == 0 {
+                sfxHeart.play()
+            } else {
+                sfxBite.play()
+            }
+        } else {
+            // monster is dead!
+            print("Trying to tend to a dead monster! You're too late!")
+            gameOver()
+        }
     }
     
     func startTimer() {
@@ -55,36 +109,87 @@ class ViewController: UIViewController {
         }
         
         timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
-        
+        //print("Timer started")
     }
 
+    func endTimer() {
+        if timer != nil {
+            timer.invalidate() // before we start a new timer, stop the existing one. safety measure.
+            //print("Timer stopped")
+        }
+    }
+    
     func changeGameState() {
         
-        penalties++
-        
-        if penalties == 1 {
-            penalty1Img.alpha = OPAQUE
-            penalty2Img.alpha = DIM_ALPHA
-        } else if penalties == 2 {
-            penalty2Img.alpha = OPAQUE
-            penalty3Img.alpha = DIM_ALPHA
-        } else if penalties == 3 {
-            penalty3Img.alpha = OPAQUE
+        if monsterAlive { // we only really want to do stuff if the monster is still alive, right?
+            
+            if !monsterHappy {
+                
+                print("Monster is dying...")
+                penalties++
+                sfxSkull.play()
+                
+                if penalties == 1 {
+                    penalty1Img.alpha = OPAQUE
+                    penalty2Img.alpha = DIM_ALPHA
+                } else if penalties == 2 {
+                    penalty2Img.alpha = OPAQUE
+                    penalty3Img.alpha = DIM_ALPHA
+                } else if penalties == 3 {
+                    penalty3Img.alpha = OPAQUE
+                } else {
+                    penalty1Img.alpha = DIM_ALPHA
+                    penalty2Img.alpha = DIM_ALPHA
+                    penalty3Img.alpha = DIM_ALPHA
+                }
+                
+                if penalties >= MAX_PENALTIES {
+                    gameOver()
+                }
+                
+            }
+            
+            let rand = arc4random_uniform(2) // 0 or 1
+            
+            if rand == 0 {
+                // let's say 0 is the heart, so dim the food icon
+                foodImg.alpha = DIM_ALPHA
+                foodImg.userInteractionEnabled = false
+                heartImg.alpha = OPAQUE
+                heartImg.userInteractionEnabled = true
+            } else {
+                heartImg.alpha = DIM_ALPHA
+                heartImg.userInteractionEnabled = false
+                foodImg.alpha = OPAQUE
+                foodImg.userInteractionEnabled = true
+            }
+            
+            currentItem = rand
+            monsterHappy = false
+            
         } else {
-            penalty1Img.alpha = DIM_ALPHA
-            penalty2Img.alpha = DIM_ALPHA
-            penalty3Img.alpha = DIM_ALPHA
-        }
-        
-        if penalties >= MAX_PENALTIES {
-            gameOver()
+            
+            print("Monster is DEAD, Dave") // we should never see this message, because when the monster dies the timer which calls this function is stopped.
+            
         }
         
     }
     
     func gameOver() {
         timer.invalidate() // we don't want the timer to go any more!
-        monsterImg.playDeathAnimation()
+        if monsterAlive {
+            // gameOver can be called when the monster first dies, and AGAIN when you try one last time to revive him, so we don't want the animation and death sfx to repeat.
+            monsterAlive = false
+            monsterImg.playDeathAnimation()
+            sfxDeath.play()
+        }
+        endTimer()
+        musicPlayer.stop()
+        foodImg.alpha = DIM_ALPHA
+        heartImg.alpha = DIM_ALPHA
+        foodImg.userInteractionEnabled = false
+        heartImg.userInteractionEnabled = false
+        print("GAME OVER!")
     }
 
 }
